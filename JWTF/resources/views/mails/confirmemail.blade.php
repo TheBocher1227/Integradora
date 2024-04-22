b8ee52ee3cd714d5fd53a7ae226394e16e8312b8
TheBocher1227 <bocher1227@gmail.com>
2024-04-21 18:54:53 -0600

    bocher

--- JWTF/resources/views/mails/confirmemail.blade.php
+++ JWTF/resources/views/mails/confirmemail.blade.php
@@ -8,18 +8,31 @@
 <body>
 
 <style>
-
-    .confirm{
-        border:1px solid black;
+    .confirm {
+        border: 1px solid black;
         border-radius: 15px;
-        background:#0BF0FF
-        
+        background: #fc2302;
+        width: 300px; 
+        height: 400px; 
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
 
+    .confirm img {
+        max-width: 100%;
+        max-height: 100%;
     }
 </style>
 
 <div class="confirm">
-<h1 style="text-align:center">Felicidades tu cuenta se ha activado</h1>
+    <img src="{{ asset('images/logo.png') }}" alt="Imagen">
+    <p style="text-align: center;">Felicidades tu cuenta se ha activado con exito</p>
 </div>
 </body>
 </html>
